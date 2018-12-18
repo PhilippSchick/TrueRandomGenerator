2655f2c000bf3ff77b5458537bc2566c66efa538
Philipp Schick <philipp.schick@studs.hs-mannheim.de>
2018-12-18 19:58:46 +0100

    added RandomGenerator.java, added a custom constructor

---
+++ src/RandomGenerator.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+
+/**
+ * @author Philipp Schick
+ * @version 1.00, 18.12.2018
+ */
+public class RandomGenerator {
+	private String email;
+
+	/**
+	 * Custom constructor for RandomGenerator, sets a email address were random.org
+	 * can contact you if your requests causes trouble
+	 * 
+	 * @param email - your email
+	 */
+	public RandomGenerator(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
